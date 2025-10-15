224bd3271774d86e05ea0bca52b5b75b48d3a173
Akbark8 <konokovakbar@gmail.com>
2025-10-16 01:01:23 +0600

    Исправлены дзшки

--- Library.kg/accounts/urls.py
+++ Library.kg/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard.html/', views.dashboard, name='dashboard.html'),
 ]
--- Library.kg/accounts/views.py
+++ Library.kg/accounts/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm, LoginForm  # Добавляем LoginForm (с капчей)
 
-
+# ---------- РЕГИСТРАЦИЯ (без капчи) ----------
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -14,7 +14,6 @@ def register(request):
                 password=form.cleaned_data['password'],
                 email=form.cleaned_data['email']
             )
-
             profile = form.save(commit=False)
             profile.user = user
             profile.save()
@@ -24,20 +23,22 @@ def register(request):
     return render(request, 'accounts/register.html', {'form': form})
 
 
+# ---------- АВТОРИЗАЦИЯ (с капчей) ----------
 def user_login(request):
-    from django.contrib.auth.forms import AuthenticationForm
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = LoginForm(request, data=request.POST)  # используем форму с капчей
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('dashboard')
     else:
-        form = AuthenticationForm()
+        form = LoginForm()
     return render(request, 'accounts/login.html', {'form': form})
 
 
+# ---------- ЛИЧНЫЙ КАБИНЕТ ----------
 @login_required
 def dashboard(request):
-    return render(request, 'accounts/dashboard', {'user': request.user})
+    return render(request, 'accounts/dashboard.html', {'user': request.user})
+
 
